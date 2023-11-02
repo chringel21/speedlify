86d7fbdb1c0d1189ec3f5c75eb52c61bf9f9682c
Christian Engel <christian.e@schindelhauerbikes.de>
2023-11-02 14:35:53 +0100

    Update metadata

--- _data/sites/chringel.js
+++ _data/sites/chringel.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: "chringel.dev", // optional, falls back to object key
-  description: "Speed testing for chringel.dev",
+  description: "Christian's personal website",
   options: {
     runs: 1,
-    frequency: 1, // (in minutes)
+    frequency: 10080, // (in minutes)
   },
   urls: ["https://chringel.dev/"],
 };
