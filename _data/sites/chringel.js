a686087102cc5b5a1f36cec1b57a75b750e93ecd
Christian Engel <christian.e@schindelhauerbikes.de>
2023-11-13 19:56:25 +0100

    Add more pages

--- _data/sites/chringel.js
+++ _data/sites/chringel.js
@@ -5,5 +5,13 @@ module.exports = {
     runs: 1,
     frequency: 10080, // (in minutes)
   },
-  urls: ["https://chringel.dev/"],
+  urls: [
+    "https://chringel.dev/",
+    "https://chringel.dev/blog/",
+    "https://chringel.dev/2022/06/creating-a-google-maps-clone-part-1-tiles-and-styles/",
+    "https://chringel.dev/about/",
+    "https://chringel.dev/notes/",
+    "https://chringel.dev/2022/07/creating-a-google-maps-clone-part-2-geocoding-or-wheres-that-place/",
+    "https://chringel.dev/2022/07/indiewebify-me-and-dont-forget-my-webmentions/",
+  ],
 };
--- _data/sites/netlify.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "https://cute-praline-01645e.netlify.app/", // optional, falls back to object key
-  description: "Christian's personal website",
-  options: {
-    runs: 1,
-    frequency: 10080, // (in minutes)
-  },
-  urls: ["https://cute-praline-01645e.netlify.app/"],
-};
